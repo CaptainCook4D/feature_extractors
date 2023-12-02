c4ca3351b29865053042336b8d2b67966110607f
rxp190007 <rxp190007@utdallas.edu>
2023-12-02 00:44:27 -0600

    From 94092

--- modalities/generate_depth_frames.py
+++ modalities/generate_depth_frames.py
@@ -42,6 +42,75 @@ class DepthEstimator:
                 align_corners=False).squeeze()
         return prediction
 
+    import os
+    import cv2
+    import multiprocessing
+    import logging
+    import torch
+    import matplotlib.pyplot as plt
+
+    class YourDepthEstimationClass:
+        def __init__(self):
+            # Initialize your depth estimation model and other setup here
+            pass
+
+        def process_video_directory(self, input_path, output_path):
+            if os.path.exists(output_path):
+                logging.info("Output path already exists: {}".format(output_path))
+                if len(os.listdir(output_path)) == len(os.listdir(input_path)):
+                    logging.info("Output path already has all the files. Skipping.")
+                    return
+                else:
+                    logging.info(f"Output: {len(os.listdir(output_path))}, Input: {len(os.listdir(input_path))} Output "
+                                 f"path does not have all the files. Deleting and recreating.")
+                    os.system("rm -rf {}".format(output_path))
+
+            os.makedirs(output_path, exist_ok=True)
+            logging.info("Processing video file: {}".format(input_path))
+
+            if input_path.endswith(".mp4") or input_path.endswith(".avi"):
+                self.extract_frames_from_video(input_path, output_path)
+            else:
+                logging.error("Input file is not a video (mp4 or avi).")
+
+            logging.info("Finished processing video file: {}".format(input_path))
+
+        def extract_frames_from_video(self, video_path, output_path):
+            cap = cv2.VideoCapture(video_path)
+            total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+            def extract_frames_worker(start, end):
+                for i in range(start, end):
+                    success, frame = cap.read()
+                    if success:
+                        frame_name = f"frame_{i:04d}.png"
+                        frame_path = os.path.join(output_path, frame_name)
+                        cv2.imwrite(frame_path, frame)
+
+            num_threads = multiprocessing.cpu_count()
+            frames_per_thread = total_frames // num_threads
+            processes = []
+
+            for i in range(num_threads):
+                start_frame = i * frames_per_thread
+                end_frame = start_frame + frames_per_thread if i < num_threads - 1 else total_frames
+                p = multiprocessing.Process(target=extract_frames_worker, args=(start_frame, end_frame))
+                processes.append(p)
+                p.start()
+
+            for p in processes:
+                p.join()
+
+            cap.release()
+
+            self.process_depth_estimation(output_path)
+
+    def process_depth_estimation(self, input_path):
+        # Implement depth estimation on extracted frames in the input_path
+        # You can use self.transform, self.device, and self.process_image_batch here
+        pass
+
+
     def process_video_directory(self, input_path, output_path):
         if os.path.exists(output_path):
             logger.info("Output path already exists: {}".format(output_path))
