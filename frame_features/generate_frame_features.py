baa03c7a550998ca11d576d3db65bf271a6206f7
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2023-12-25 13:36:22 +0400

    Update generate_frame_features.py

--- frame_features/generate_frame_features.py
+++ frame_features/generate_frame_features.py
@@ -50,7 +50,7 @@ class TSMFeatureExtractor(nn.Module):
 
         # ResNet-101 Neural Network for feature extraction
         network = models.resnet101(weights = models.ResNet101_Weights.IMAGENET1K_V1)
-        modules = list(network.children())[:-2]  # Remove the last fully connected layer and avgpool
+        modules = list(network.children())[:-1]  # Remove the last fully connected layer
         self.resnet101 = nn.Sequential(*modules)
         self.pca_2048 = PCA(n_components=2048)
         for param in self.resnet101.parameters():
