9ed947c4c36e9548f4c08268f6b2a2cc97627052
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2023-12-26 16:41:30 +0400

    Update test.py

--- frame_features/test.py
+++ frame_features/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Load the .npz file
-npz_file = np.load('/data/rohith/captain_cook/features/gopro/tsm/10_50_360p/frame_000000.jpg.npz')
+npz_file = np.load('/data/rohith/captain_cook/features/gopro/frames/tsm/10_50_360p/frame_000000.jpg.npz')
 
 # List all files/arrays in the npz file
 print("Contents of the NPZ file:")
