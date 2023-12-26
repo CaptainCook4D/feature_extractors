f248daf4978861b6d7aa52b7dd0152a1187cb8c6
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2023-12-26 23:58:18 +0400

    Update generate_frame_features.py

--- frame_features/generate_frame_features.py
+++ frame_features/generate_frame_features.py
@@ -145,10 +145,9 @@ def process_batch(video_name, root, frames_batch, output_features_path, feature_
         else:
             extracted_features_np = extracted_features
 
-        
-        feature_map[frames_batch[0]] = extracted_features_np
+        feature_map.append(extracted_features_np)
         #print(f"Features for {frames_batch[0]}: {feature_map}")
-        print("len of feature map: ", len(feature_map.keys()))
+        print("len of feature map: ", len(feature_map))
         if len(feature_map.keys()) == total_files(video_name):
             feature_file_path = os.path.join(output_features_path, f"{video_name}.npz")
             np.savez(feature_file_path, feature_map)
@@ -173,7 +172,7 @@ def main(n_segment, video_frames_directories_path, output_features_path, batch_s
     queue = Queue()
 
     threads = []
-    feature_map = {}
+    feature_map = []
     for _ in range(num_worker_threads):
         t = Thread(target=worker, args=(queue,output_features_path,))
         t.start()
