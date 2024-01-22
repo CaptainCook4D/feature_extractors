4258d436949eef5a724fe53cdd2e7b2c6f660ec6
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-01-21 23:09:27 -0600

    Update generate_frame_features.py

--- frame_features/generate_frame_features.py
+++ frame_features/generate_frame_features.py
@@ -115,13 +115,13 @@ class Processor():
     @staticmethod
     def process_video(video_name, video_frames_directories_path, output_features_path, tsm_extractor):
         try:
-            print("Executing processor ==========")
             video_directory = os.path.join(video_frames_directories_path, video_name)
             feature_path = os.path.join(output_features_path,  video_name)
             frames = sorted(os.listdir(video_directory), key=lambda x: int(x.split("_")[1][:-4]))
             batch_size = 1000
             video_features = []
             for i in tqdm(range(0, len(frames), batch_size), desc=f"TSM Feature Extraction for video: {video_name}"):
+                print("Executing processor ==========")
                 batch_frames = []
                 batch_names = []
                 for frame in frames[i:i+batch_size]:
