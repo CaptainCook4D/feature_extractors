bb651c228fdec3699128530e81b5dfbf1910be1a
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-01-23 15:04:28 -0600

    Update generate_frame_features.py

--- frame_features/generate_frame_features.py
+++ frame_features/generate_frame_features.py
@@ -66,7 +66,7 @@ class TSMFeatureExtractor():
         x = x.to(device)
         N, T, C, H, W = x.size()
 
-        x = x.view(N * T, C, H, W)
+        #x = x.view(N * T, C, H, W)
 
         shifted_features = self.temporal_shift(x)
         features = self.resnet101(shifted_features)
