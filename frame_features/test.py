7104436e7f06e732de1ab30f6b53ea9bc76af488
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-01-23 18:12:00 -0600

    Update test.py

--- frame_features/test.py
+++ frame_features/test.py
@@ -3,8 +3,9 @@ import numpy as np
 # Load the .npz file
 npz_file = np.load('/data/rohith/captain_cook/features/gopro/frames/omnivore/10_16_360p.npz')
 
+
 # List all files/arrays in the npz file
 print("Contents of the NPZ file:")
 for file in npz_file.files:
     print(file)
-    print(npz_file[file].size)
+    print(npz_file[file].size[0], npz_file[file].size[1])
